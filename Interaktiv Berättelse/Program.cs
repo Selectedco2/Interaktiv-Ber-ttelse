89f4f285136935f46b6bbe1927f745ba6a4ce276
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-09-07 11:04:29 +0200

    Lektion 1. Interaktiv berättekse

---
+++ Interaktiv Berättelse/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Du vaknar upp i mitten av en grotta, utan minne om hur du kom dit. Framför dig delas grottan i tre. Väljer du att fortsätta rakt fram eller vika av till vänster eller höger? ");
+string direction = Console.ReadLine();
+
+if (direction == "vänster")
+{
+    
+    Console.WriteLine("Du bestämmer dig för att gå vänster. Du fortsätter att gå framåt. Du fortsäter gå för vad som känns som en evighet, tillsut kommer du fram till en dörr som är större än du någonsin sätt förut. ");
+    Console.WriteLine("Dörren har ett pentagram framför det, vad vill du göra? [gå tillbaka / offra blod]");
+    
+    string offring = Console.ReadLine();
+     
+     if (offring == "offra blod")
+     {
+
+     }
+}
+
+
+
+Console.ReadLine();
